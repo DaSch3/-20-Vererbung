e6842fb594b361ca7b5f51801f6eac23ed386556
David Schmitz <d.schmitz@dasch3.de>
2021-09-04 19:40:48 +0200

    Projektdateien hinzufügen.

---
+++ Vererbung/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vererbung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hund dog = new Hund();
+
+            dog.Name = "Coco";
+            dog.Bellen();
+            dog.Geschlecht = "Rüde";
+            dog.Bewegen();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Tier
+    {
+        //Eigenschaften
+        public string Geschlecht { get; set; }
+
+        //Methoden
+        public void Bewegen()
+        {
+            Console.WriteLine("Bewegen...");
+        }
+    }
+
+    class Hund : Tier
+    {
+        //Eigenchaften
+        public string Name { get; set; }
+
+        //Methoden
+        public void Schnüffeln()
+        {
+            Console.WriteLine("Schnüfflen...");
+        }
+        public void Bellen()
+        {
+            Console.WriteLine("Bellen...");
+        }
+    }
+}
